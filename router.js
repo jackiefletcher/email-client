ecbf41442f609acefd2d54f39a316a092bc28183
Jackie Fletcher <jaxskriv@gmail.com>
2015-03-06 11:02:40 -0800

    add link to view email message

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 Email.Router.map(function() {
   this.resource('emails', {path: '/'});
-  this.resource('message', {path: 'email_id'});
+  this.resource('message', {path: ':email_id'});
   this.resource('new-email');
 });
